bcd38984bc7e230abab91dfe243f0291f36a5ac3
Inam Ullah Yousufzai <58142833+inamsaif0@users.noreply.github.com>
2023-10-12 17:24:09 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,18 @@
+// Import the Express.js library
+const express = require('express')
 
+// Create an instance of the Express application
+const app = express();
 
-// Import modules or libraries
-// const http = require('http');
-
-// Create a simple HTTP server
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-//   res.end('Hello, World!\n');
-// });
+// Define a route that responds with "Hello, World!"
+app.get('/', (req, res) => {
+  res.send('Hello, World!\n');
+});
 
 // Define the port the server will listen on
-
-
 const port = process.env.PORT || 3000;
 
 // Start the server
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
